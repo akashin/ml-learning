2d82a16526674fd54943da96893ac8a90b145500
akashin <kashin.andrej@gmail.com>
2019-03-17 14:27:34 +0000

    Add LSTM XOR task

--- request_for_research/lstm.py
+++ request_for_research/lstm.py
@@ -2,32 +2,47 @@ import tensorflow as tf
 import tensorflow.contrib.eager as tfe
 import numpy as np
 
-class GRU(tf.keras.layers.Layer):
-    """Gated recurrent unit."""
+class LSTM(tf.keras.layers.Layer):
+    """Long Short Term Memory network module."""
     
-    def __init__(self, hidden_size):
-        self.hidden_size = hidden_size
-        # self.hidden_state = tfe.Variable(np.zeros(self.hidden_size), dtype=tf.float32)
-        super(GRU, self).__init__()
+    def __init__(self):
+        super(LSTM, self).__init__()
     
-    def build(self, input_shape):
-        print(input_shape)
-        input_size = input_shape[-1]
-        self.W_z = self.add_variable("W_z", shape=[self.hidden_size, self.hidden_size + input_size], 
+    def build(self, shapes):
+        x_shape, h_shape, c_shape = shapes
+        x_size = x_shape[-1]
+        h_size = h_shape[-1]
+        c_size = c_shape[-1]
+        
+        assert(h_size == c_size)
+        
+        # Forget gate.
+        self.W_fg = self.add_variable("W_fg", shape=[c_size, x_size + h_size], 
                                      initializer="uniform")
-        self.W_r = self.add_variable("W_r", shape=[self.hidden_size, self.hidden_size + input_size],
+        
+        # Input gate and input.
+        self.W_ig = self.add_variable("W_ig", shape=[c_size, x_size + h_size], 
                                      initializer="uniform")
-        self.W = self.add_variable("W", shape=[self.hidden_size, self.hidden_size + input_size],
-                                   initializer="uniform")
+        self.W_i = self.add_variable("W_i", shape=[c_size, x_size + h_size], 
+                                     initializer="uniform")
+        
+        # Output gate.
+        self.W_og = self.add_variable("W_og", shape=[c_size, x_size + h_size], 
+                                     initializer="uniform")
+        
+        super(LSTM, self).build(shapes)
     
-    def call(self, input):
-        print(input.dtype)
-        print(self.hidden_state.dtype)
-        z = tf.sigmoid(self.W_z * tf.stack([self.hidden_state, input]))
-        r = tf.sigmoid(self.W_z * tf.stack([self.hidden_state, input]))
-        h_hat = tf.tanh(self.W * tf.stack([r * self.hidden_state, input]))
-        self.hidden_state = (1 - z) * self.hidden_state + z * h_hat
-        return self.hidden_state
-    
-    #def compute_output_shape(self, input_shape):
-        #return (input_shape[0], self.hidden_size)
+    def call(self, inputs):
+        x, h, c = inputs
+        
+        xh = tf.concat([x, h], 1)
+        
+        fg = tf.sigmoid(tf.matmul(xh, self.W_fg, transpose_b=True))
+        ig = tf.sigmoid(tf.matmul(xh, self.W_ig, transpose_b=True))
+        i = tf.tanh(tf.matmul(xh, self.W_ig, transpose_b=True))
+        nc = fg * c + ig * i
+        
+        og = tf.sigmoid(tf.matmul(xh, self.W_og, transpose_b=True))
+        nh = og * tf.tanh(nc)
+        
+        return nh, nc
